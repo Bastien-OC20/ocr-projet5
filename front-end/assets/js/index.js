5af63ef96a3f0388c99af1859d68d91d44675535
sebastien <bastien.rapuzzi@gmail.com>
2020-11-24 22:34:37 +0100

    🔨 Ajout des  liens des cameras

--- front-end/assets/js/index.js
+++ front-end/assets/js/index.js
@@ -11,37 +11,40 @@ fetch('https://oc-p5-api.herokuapp.com/api/cameras')
                   for (let i = 0; i < products.length; i++) {
                     let myArticle = document.createElement('article')
     allProducts.appendChild(myArticle) 
-myArticle.setAttribute('class', 'card')
+
 let img = document.createElement('img')
 let H3 = document.createElement('h3')
-
 let P1 = document.createElement('p')
-let button = document.createElement('BUTTON')
+let button = document.createElement('button')
+let link = document.createElement('a')
 
 img.src = products[i].imageUrl
 H3.textContent =  products[i].name
 P1.textContent = 'Prix: '+ (products[i].price / 100)+ '€'
 button.textContent = 'Voir le produit'
+link.href = products[i]._id
 
 myArticle.appendChild(img)
 myArticle.appendChild(H3)
 myArticle.appendChild(P1)
 myArticle.appendChild(button)
+button.appendChild(link)
 
 
-myArticle.setAttribute('class', 'padding:3em  products  card col-lg-4  m-5')
+myArticle.setAttribute('class', 'products card col-lg-4 p-3 m-5')
 img.setAttribute('class', 'image card-img-top p-3')
 img.setAttribute('title', products[i].name)
 H3.setAttribute('class', 'product card-title')
 P1.setAttribute('class', 'price card-text')
-button.setAttribute('class','btn btn-light mb-3 w-4')
+button.setAttribute('class','btn btn-light w-50 m-auto')
+
 }
       console.log('connecté au serveur');
     
 }
     // Why  error ?.....
               } else {
-                  reject(console.log('erreur :' + error));
+                  reject(console.log(error));
               }
           }
           
